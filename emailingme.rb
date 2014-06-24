41d57859a77f1e93d06c5afe0ebb9cf3b89a93a1
emmaladouceur <eladouceur@brearley.org>
2014-06-24 10:43:46 -0400

    methods

--- emailingme.rb
+++ emailingme.rb
@@ -2,19 +2,25 @@ require 'mailgun'
 
 class Email
 
-Mailgun.configure do |config|
+  Mailgun.configure do |config|
   config.api_key = 'key-0adwu1qqkvew8h2-unmvynmqdjc-2dv1'
   config.domain  = 'sandbox644864e1e7ea479586422a0b6e9b390a.mailgun.org'
+  end
+
+  def send   
+    @mailgun = Mailgun(:api_key => 'key-0adwu1qqkvew8h2-unmvynmqdjc-2dv1')
+
+    parameters = {
+    :to => "eladouceur@mail.brearley.org",
+    :subject => "testing",
+    :text => "testing",
+    :from => "postmaster@sandbox644864e1e7ea479586422a0b6e9b390a.mailgun.org"
+    }
+    @mailgun.messages.send_email(parameters)
+  end
+
 end
 
-@mailgun = Mailgun(:api_key => 'key-0adwu1qqkvew8h2-unmvynmqdjc-2dv1')
+my_email = Email.new
 
-parameters = {
-  :to => "eladouceur@mail.brearley.org",
-  :subject => "testing",
-  :text => "testing",
-  :from => "postmaster@sandbox644864e1e7ea479586422a0b6e9b390a.mailgun.org"
-}
-@mailgun.messages.send_email(parameters)
-
-end
+my_email.send
